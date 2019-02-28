bb4124d2c8b620e695fc479abc506695b01250ee
Jarosław Waszczuk <jarekwaszczuk@poczta.fmp>
2019-02-28 11:32:21 +0100

    Ładowanie pliku

---
+++ src/ZadanieKonkurs4developers.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ZadanieKonkurs4developers {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("dane/test_input.txt");
+        Scanner in = new Scanner(file);
+
+
+    }
+}
