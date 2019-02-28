3b86c1d12f39358de58997e04590a7da9b2644fa
Jarosław Waszczuk <jarekwaszczuk@poczta.fmp>
2019-02-28 13:18:29 +0100

    Działające wyliczenie. Kod do upiększenia.

--- src/ZadanieKonkurs4developers.java
+++ src/ZadanieKonkurs4developers.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ZadanieKonkurs4developers {
@@ -8,7 +9,7 @@ public class ZadanieKonkurs4developers {
         File file = new File("dane/test_input.txt");
         Scanner in = new Scanner(file);
 
-        ArrayList[] data= new ArrayList[1000];
+        ArrayList<ArrayList<Integer>> data = new ArrayList<>();
 
         int counter = 0;
         while (in.hasNext()) {
@@ -22,21 +23,42 @@ public class ZadanieKonkurs4developers {
             list.add(Integer.parseInt(split[2]));
 
             list.sort(Integer::compareTo);
-            data[counter] = list;
+            data.add(list);
             counter++;
 
 //            //diagnostyka wczytanych danych
 //            System.out.print(counter+" =>\t");
-//            System.out.print(data[counter-1].get(0));
+//            System.out.print(data.get(counter-1).get(0));
 //            System.out.print("\t");
-//            System.out.print(data[counter-1].get(1));
+//            System.out.print(data.get(counter-1).get(1));
 //            System.out.print("\t");
-//            System.out.print(data[counter-1].get(2));
+//            System.out.print(data.get(counter-1).get(2));
 //            System.out.println();
-
-
-
         }
 
+        System.out.println("Wyliczone pole: " + calculateAreaWithAddition(data) +" dm2");
+    }
+
+    public static int calculateAreaWithAddition(ArrayList<ArrayList<Integer>> data) {
+        int area = 0;
+
+        for (ArrayList<Integer> list : data) {
+
+//            System.out.println(list.toString());
+            int currentArea = 0;
+            int dimensionA = Integer.valueOf(list.get(0).toString());
+            int dimensionB = Integer.valueOf(list.get(1).toString());
+            int dimensionC = Integer.valueOf(list.get(2).toString());
+
+            currentArea = 2 * (dimensionA * dimensionB) +
+                    2 * (dimensionB * dimensionC) +
+                    2 * (dimensionC * dimensionA) +
+                    dimensionA * dimensionB
+            ;
+
+            area += currentArea;
+        }
+
+        return area;
     }
 }
